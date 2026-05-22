3f150abd20a278b31968024fab30a40949ab9260
aaryatopiwala <75400069+aaryatopiwala@users.noreply.github.com>
2026-05-21 19:23:06 -0700

    fix present ordering

--- cpu/present/present_ctr.cpp
+++ cpu/present/present_ctr.cpp
@@ -13,14 +13,14 @@ static void encrypt_ctr_buffer(const uint8_t *plaintext, uint8_t *ciphertext, si
         uint64_t cipher_block;
         present80_encrypt(&block, key, &cipher_block);
 
-        ciphertext[i] = ((cipher_block >> 56) & 0xFF) ^ plaintext[i];
-        ciphertext[i+1] = ((cipher_block >> 48) & 0xFF) ^ plaintext[i+1];
-        ciphertext[i+2] = ((cipher_block >> 40) & 0xFF) ^ plaintext[i+2];
-        ciphertext[i+3] = ((cipher_block >> 32) & 0xFF) ^ plaintext[i+3];
-        ciphertext[i+4] = ((cipher_block >> 24) & 0xFF) ^ plaintext[i+4];
-        ciphertext[i+5] = ((cipher_block >> 16) & 0xFF) ^ plaintext[i+5];
-        ciphertext[i+6] = ((cipher_block >> 8) & 0xFF) ^ plaintext[i+6];
-        ciphertext[i+7] = (cipher_block & 0xFF) ^ plaintext[i+7];
+        ciphertext[i] = ((cipher_block >> 0) & 0xFF) ^ plaintext[i];
+        ciphertext[i+1] = ((cipher_block >> 8) & 0xFF) ^ plaintext[i+1];
+        ciphertext[i+2] = ((cipher_block >> 16) & 0xFF) ^ plaintext[i+2];
+        ciphertext[i+3] = ((cipher_block >> 24) & 0xFF) ^ plaintext[i+3];
+        ciphertext[i+4] = ((cipher_block >> 32) & 0xFF) ^ plaintext[i+4];
+        ciphertext[i+5] = ((cipher_block >> 40) & 0xFF) ^ plaintext[i+5];
+        ciphertext[i+6] = ((cipher_block >> 48) & 0xFF) ^ plaintext[i+6];
+        ciphertext[i+7] = ((cipher_block >> 56) & 0xFF) ^ plaintext[i+7];
 
     }
 }
